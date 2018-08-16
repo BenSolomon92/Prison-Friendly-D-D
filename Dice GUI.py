6be0d0f084120afc595e7594d04bd5b6527ef69e
Solomon <bensolomon89@gmail.com>
2018-08-16 09:45:20 -0400

    Added a program title and stops kernel from dying if multiple windows are generated in the same session

--- Dice GUI.py
+++ Dice GUI.py
@@ -7,10 +7,11 @@ Created on Wed Aug 15 13:56:59 2018
 
 """Check"""
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5.QtGui import QIcon
- 
-class App(QWidget):
+
+
+class App(QMainWindow):
  
     def __init__(self):
         super().__init__()
@@ -24,9 +25,13 @@ class App(QWidget):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.statusBar().showMessage("DnD Dicerolls")
         self.show()
  
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QApplication.instance()
+    #Stops kernel from dying if the windows is generated multiple times
+    if app is None:
+        app = QApplication(sys.argv)
     ex = App()
     sys.exit(app.exec_())
