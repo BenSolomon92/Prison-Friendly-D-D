1229ece4fd07660f71cb81da7ca08adba9f6af27
Solomon <bensolomon89@gmail.com>
2018-08-19 16:00:16 -0400

    Created a button on the window

--- Dice GUI.py
+++ Dice GUI.py
@@ -7,7 +7,8 @@ Created on Wed Aug 15 13:56:59 2018
 
 """Check"""
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QIcon
 
 
@@ -15,7 +16,7 @@ class App(QMainWindow):
  
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 simple window - pythonspot.com'
+        self.title = 'DnD Dicerolls'
         self.left = 10
         self.top = 10
         self.width = 640
@@ -25,9 +26,19 @@ class App(QMainWindow):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        button = QPushButton('PyQt5 button', self)
+        button.setToolTip('This is an example button')
+        button.move(100,70)
+        button.clicked.connect(self.on_click)
+        
         self.statusBar().showMessage("DnD Dicerolls")
         self.show()
  
+    @pyqtSlot()
+    def on_click(self):
+        print('PyQt5 button click')
+    
 if __name__ == '__main__':
     app = QApplication.instance()
     #Stops kernel from dying if the windows is generated multiple times
