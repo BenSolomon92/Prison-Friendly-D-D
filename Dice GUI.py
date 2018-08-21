27941fb6e571d99c148ce34445f8eff878f49df8
Solomon <bensolomon89@gmail.com>
2018-08-20 21:47:46 -0400

    Added an initial message handler before getting into the program

--- Dice GUI.py
+++ Dice GUI.py
@@ -7,9 +7,8 @@ Created on Wed Aug 15 13:56:59 2018
 
 """Check"""
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QMessageBox
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtGui import QIcon
 
 
 class App(QMainWindow):
@@ -32,12 +31,19 @@ class App(QMainWindow):
         button.move(100,70)
         button.clicked.connect(self.on_click)
         
+        buttonReply = QMessageBox.question(self, 'PyQt5 message', "Do you like PyQt5?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if buttonReply == QMessageBox.Yes:
+            print('Yes clicked.')
+        else:
+            print('No clicked.')
+        
         self.statusBar().showMessage("DnD Dicerolls")
         self.show()
  
     @pyqtSlot()
     def on_click(self):
         print('PyQt5 button click')
+
     
 if __name__ == '__main__':
     app = QApplication.instance()
